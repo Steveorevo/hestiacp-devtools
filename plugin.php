087173501a580f61947c5262624e9fb4f589a224
Stephen J. Carnam <steve@steveorevo.com>
2023-01-30 17:36:20 -0800

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: DevTools
+ * Plugin URI: https://github.com/steveorevo/hestiacp-devtools
+ * Description: Automatically furnishes composer, wp-cli, etc. for each user account that is created.
+ */
+
