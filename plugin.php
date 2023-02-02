a084cdfb8b041a609785e6323edc83d553bb7626
Stephen J Carnam <steveorevo@gmail.com>
2023-02-02 12:57:09 -0800

    reg. scripts

--- plugin.php
+++ plugin.php
@@ -5,12 +5,14 @@
  * Description: Automatically furnishes composer, wp-cli, etc. for each user account that is created.
  */
 
- global $hcpp;
+global $hcpp;
+$hcpp->register_install_script( dirname(__FILE__) . '/install' );
+$hcpp->register_uninstall_script( dirname(__FILE__) . '/uninstall' );
 
- // Execute the devtools.sh script asychronously when a new user is created.
- $hcpp->add_action( 'priv_add_user', function( $args) {
-    $user = $args[0];
-    $cmd = "nohup " . __DIR__ . "/devtools.sh ";
-    $cmd .= escapeshellarg( $user ) . ' > /dev/null 2>&1 &';
-    shell_exec( $cmd );
- });
+// Execute the devtools.sh script asychronously when a new user is created.
+$hcpp->add_action( 'priv_add_user', function( $args) {
+   $user = $args[0];
+   $cmd = "nohup " . __DIR__ . "/devtools.sh ";
+   $cmd .= escapeshellarg( $user ) . ' > /dev/null 2>&1 &';
+   shell_exec( $cmd );
+});
